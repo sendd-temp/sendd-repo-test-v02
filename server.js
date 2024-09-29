44c582daf15dc5e60a4eb3a704baaf65afe52567
send.adm@gmail.com <send.adm@gmail.com>
2024-09-29 21:04:31 +0400

    Your descriptive commit message

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 
 
 // --------------
-// Transactions
+// Assets
 // --------------
 
 // Fetch all assets
@@ -53,6 +53,45 @@ app.post('/api/assets', async (req, res) => {
     }
 })
 
+
+
+
+
+
+// --------------
+// Transactions
+// --------------
+
+// Fetch all transactions
+app.get('/api/transactions', async(req, res) => {
+    try {
+        const allItems = await itemsPool.query(
+            'SELECT * FROM transactions'
+        );
+        res.json({ allItems });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
+    }
+})
+
+// Create new transaction
+app.post('/api/transactions', async (req, res) => {
+    const { account_from, account_to, amount_from_fullunits, amount_to_fullunits, asset_id_from, asset_id_to, transaction_description, transaction_code } = req.body;
+    try {
+        const newItem = await itemsPool.query(
+            'INSERT INTO transactions (account_from, account_to, amount_from_fullunits, amount_to_fullunits, asset_id_from, asset_id_to, transaction_description, transaction_code) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+            [account_from, account_to, amount_from_fullunits, amount_to_fullunits, asset_id_from, asset_id_to, transaction_description, transaction_code]
+        );
+        res.json({ 
+            message: "New transaction added!",
+            item: newItem.rows
+         });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error.message)
+    }
+})
 
 
 
